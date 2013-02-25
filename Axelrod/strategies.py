34f7a769e1bf306db154a0856b376b4236b5a97d
Zachary Allaun <zach.allaun@gmail.com>
2013-02-25 16:17:46 -0500

    Refactor strategies
    Sorry, this isn't a particularly atomic commit. Changes in no particular
    order:
    
    - Tabs to spaces and remove some trailing whitespace.
    - Inline if/else statements.
    - In `stubborn`, the use of `while` didn't really make sense, as it
      wasn't ever actually iterating; it either returned immediately or
      moved onto the else branch. I just moved the condition up to the
      containing `if` and or'd it.

--- Axelrod/strategies.py
+++ Axelrod/strategies.py
@@ -5,57 +5,47 @@ from random import randint, choice
 
 def tit_for_tat(iteration, opp_last_move):
     """Cooperate first, then play opponent's last move"""
-    if iteration == 0: return 'C'
-    return opp_last_move
+    return 'C' if iteration == 0 else opp_last_move
 
 def always_C(iteration, opp_last_move):
-	"""Always cooperate"""
-	return 'C'
+    """Always cooperate"""
+    return 'C'
 
 def always_D(iteration, opp_last_move):
-	"""Always defect"""
-	return 'D'
+    """Always defect"""
+    return 'D'
 
 def randomizer(iteration, opp_last_move):
-	"""Play a random strategy"""
-	return choice(['D','C'])
+    """Play a random strategy"""
+    return choice(['D', 'C'])
 
 def switch_C(iteration, opp_last_move):
-	"""Start with C, switch strategies after that."""
-	if iteration%2 == 0: return 'C'
-	return 'D'
+    """Start with C, switch strategies after that."""
+    return 'C' if iteration % 2 == 0 else 'D'
 
 def switch_D(iteration, opp_last_move):
-	"""Start with D, switch strategies after that."""
-	if iteration%2 == 0: return 'D'
-	return 'C'
+    """Start with D, switch strategies after that."""
+    return 'D' if iteration % 2 == 0 else 'C'
 
 def nasty_tft(iteration, opp_last_move):
     """Defect first, then play opponent's last move"""
-    if iteration == 0: return 'D'
-    return opp_last_move
+    return 'D' if iteration == 0 else opp_last_move
 
 def stubborn(iteration, opp_last_move):
     """Cooperate, until opponent defects. Then always defect"""
-    if iteration == 0: return 'C'
+    if iteration == 0 or opp_last_move == 'C':
+        return 'C'
     else:
-        while opp_last_move == 'C':
-            return 'C'
-        else:
-            return 'D'
+        return 'D'
 
 def play_70C(iteration, opp_last_move):
     """Cooperate 70% of the time."""
-    integer = randint(0, 9)
-    if integer<7: return 'C'
-    return 'D'
+    return 'C' if randint(0, 9) < 7 else 'D'
 
 def play_70D(iteration, opp_last_move):
     """Defect 70% of the time."""
-    integer = randint(0, 9)
-    if integer<7: return 'D'
-    return 'C'
+    return 'D' if randint(0, 9) < 7 else 'C'
 
-all = {'tft': tit_for_tat, 'ac': always_C, 'ad': always_D, 'rand': randomizer, 
+all = {'tft': tit_for_tat, 'ac': always_C, 'ad': always_D, 'rand': randomizer,
        'c_switch': switch_C, 'd_switch': switch_D, 'ntft': nasty_tft,
        'c_until_d': stubborn, 'c_70': play_70C, 'd_70': play_70D}
